6c5c9c854bf3433e6640a630c0e66ab00e4ad4c4
noraalobaidi <nalobaidi_@hotmail.com>
2022-12-19 10:00:49 +0300

    CREATE project

---
+++ src/main/java/com/nora/project3/ProjectPhaseThreeApplication.java
@@ -0,0 +1,13 @@
+package com.nora.project3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPhaseThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPhaseThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nora/project3/ProjectPhaseThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nora.project3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPhaseThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
